4aeec95bcf19889b594722182126b0060a93242c
Mitchell Stanton-Cook <mscook@gmail.com>
2013-06-18 17:38:33 +1000

    Config tweaking

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,9 +17,9 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('GitHub', 'https://github.com/mscook/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),)
+LINKS =  (('LinkedIn', 'http://au.linkedin.com/in/mjstantoncook'),
+          ('Scholar',  'http://scholar.google.com/citations?user=MGafrX4AAAAJ&hl=en'),
+          ('GitHub',   'http://github.com/mscook/'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -28,8 +28,18 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 THEME = os.path.expanduser("~/3rd-repos/pelican-themes/chunk")
-SITESUBTITLE = 'Bioinformatics, Software Developemnt & System Administration'
+SITESUBTITLE = 'Biology, Code & Computers'
 FOOTER_TEXT = '(c) 2013 Mitchell Stanton-Cook'
 SINGLE_AUTHOR = True
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+DISPLAY_PAGES_ON_MENU = True
+
+GITHUB_URL = 'http://github.com/mscook'
+#Note - real is Bio.Dev.Op but giving this seems to override shortname...
+DISQUS_SITENAME = 'biodevop'
+DISQUS_SHORTNAME = 'biodevop'
+
+#FILES_TO_COPY = (
+#    ('CNAME/CNAME', 'CNAME'),)
