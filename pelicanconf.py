2cfd4f63a34299eddf429a9c0f865c59d0d3ece0
Mitchell Stanton-Cook <mscook@gmail.com>
2013-08-14 00:11:26 +1000

    Tweaks

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,10 +5,11 @@ import os
 
 AUTHOR = u'Mitchell Stanton-Cook'
 SITENAME = u'Bio.Dev.Op'
-SITEURL = ''
+SITESUBTITLE = 'Biology, Code & Computers'
+SITEDESCRIPTION = "Bio.Dev.Op - Biology, Code & Computers"
+SITEURL = 'http://mscook.github.io/'
 
 TIMEZONE = 'Australia/Brisbane'
-
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
@@ -17,9 +18,7 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('LinkedIn', 'http://au.linkedin.com/in/mjstantoncook'),
-          ('Scholar',  'http://scholar.google.com/citations?user=MGafrX4AAAAJ&hl=en'),
-          ('GitHub',   'http://github.com/mscook/'),)
+LINKS =  ()
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -28,7 +27,6 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 3
 
 THEME = os.path.expanduser("~/3rd-repos/spoonbill")
-SITESUBTITLE = 'Biology, Code & Computers'
 FOOTER_TEXT = '(c) 2013 Mitchell Stanton-Cook'
 SINGLE_AUTHOR = True
 # Uncomment following line if you want document-relative URLs when developing
@@ -36,16 +34,16 @@ SINGLE_AUTHOR = True
 
 DISPLAY_PAGES_ON_MENU = True
 
-GITHUB_URL = 'https://github.com/mscook'
-TWITTER_USERNAME = 'mscook'
-#Note - real is Bio.Dev.Op but giving this seems to override shortname...
-DISQUS_SITENAME = 'biodevop'
-DISQUS_SHORTNAME = 'biodevop'
-
-#Spoonbill specific
-SITEDESCRIPTION = "Bio.Dev.Op - Biology, Code & Computers"
+#### External services ###
 GOOGLE_WEBMASTER = "tYDBAsj8peJGDx84OVpLivF0Q8n0Q94PaL38KKLPLdg"
-TWITTER_URL = "https://twitter.com/mscook"
+GOOGLE_ANALYTICS = 'UA-43147617-1'
+TWITTER_URL      = "https://twitter.com/mscook"
+TWITTER_USERNAME = 'mscook'
+# Note - real is Bio.Dev.Op but giving this seems to override shortname...
+DISQUS_SITENAME  = 'biodevop'
+DISQUS_SHORTNAME = 'biodevop'
+GITHUB_URL       = 'https://github.com/mscook'
+
 
 #FILES_TO_COPY = (
 #    ('CNAME/CNAME', 'CNAME'),)
